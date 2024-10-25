499e4facbf98dc87bbdb3dad1697afa4567c2398
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-10-25 15:39:34 +0200

    project structure

---
+++ ShoppingCart/ShoppingCart.Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ShoppingCart.Application.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ShoppingCart/ShoppingCart.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ShoppingCart.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
