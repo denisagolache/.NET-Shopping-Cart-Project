8b109929d97e744cb5760a48d204780ae44eee38
Vreme Iulia <ivreme12@gmail.com>
2024-10-27 01:31:03 +0300

    Șterg folderul obj din Application

--- ShoppingCart/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ ShoppingCart/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ff7729c4896733fdd74707dbcd647256b197ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b9390ae8fd7d14b92584d47a8b62ea001d3cea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
